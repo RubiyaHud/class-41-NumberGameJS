639d101ecde43bb7fff9fdce58535a100bd26b72
RubiyaHud <rubiya.hud@gmail.com>
2025-03-14 17:44:33 +0600

    class-43

--- js/script.js
+++ js/script.js
@@ -12,6 +12,13 @@ let playerPlayBtn = document.querySelector(".playerPlayBtn");
 let playerOneError = document.querySelector(".playerOneError");
 // ============================================================
 
+// ======================= Player Two =========================
+let playerTwoGameArea = document.querySelector(".playerTwoGameArea");
+let playerTwoNameInput = document.querySelector(".playerTwoNameInput");
+let playerTwoPlayBtn = document.querySelector(".playerTwoPlayBtn");
+let playerTwoNameError = document.querySelector(".playerTwoNameError");
+// ============================================================
+
 playBtn.addEventListener("click", function () {
     if (nameInput.value == "") {
         nameError.style.display = "block";
@@ -28,6 +35,35 @@ playBtn.addEventListener("click", function () {
 playerPlayBtn.addEventListener("click", function () {
     if (playerOneInput.value == "") {
         playerOneError.style.display = "block";
+        playerOneError.innerHTML = "Number is Missing";
+    } 
+    else if(playerOneInput.value <1 || playerOneInput.value > 10)   {
+        playerOneError.style.display = "block";
+        playerOneError.innerHTML = "Out of Range!";
+    }
+    else if (playerOneInput.value - 15) {
+        console.log("It's a number");
+        playerOnegameArea.style.display = "none";
+        playerTwoGameArea.style.display = "block";
+        
+    }
+    else{
+        playerOneError.style.display = "block";
+        playerOneError.innerHTML = "Invalid input";
+    }
+    
+});
+// ============================================================
+// ======================= Player Two =========================
+playerTwoPlayBtn.addEventListener("click", function () {
+    if (playerTwoNameInput.value == "") {
+        playerTwoNameError.style.display = "block";
+    }
+    else {
+        playerTwoGameArea.style.display = "none";
+        // playerOnegameArea.style.display = "block";
+        // let h = playerOneHeading.innerHTML + ' ( ' + nameInput.value + ' ) ';
+        // playerOneHeading.innerHTML = h;
     }
 });
 // ============================================================
