01ffbcf91fc95ddff56b95108ae9dc6eb2181e15
RubiyaHud <rubiya.hud@gmail.com>
2025-03-15 14:13:23 +0600

    Add number guess chances

--- js/script.js
+++ js/script.js
@@ -18,6 +18,15 @@ let playerTwoNameInput = document.querySelector(".playerTwoNameInput");
 let playerTwoPlayBtn = document.querySelector(".playerTwoPlayBtn");
 let playerTwoNameError = document.querySelector(".playerTwoNameError");
 // ============================================================
+// ======================= Player Two Chance =========================
+let playerTwoChanceOne = document.querySelector(".playerTwoChanceOne");
+let playerTwoHeading = document.querySelector(".playerTwoHeading");
+let playerTwoChanceNum = document.querySelector(".playerTwoChanceNum");
+let playerTwoChanceOneInput = document.querySelector(".playerTwoChanceOneInput");
+let playerTwoChanceOnePlayBtn = document.querySelector(".playerTwoChanceOnePlayBtn");
+let playerTwoChanceOneError = document.querySelector(".playerTwoChanceOneError");
+let counter = 1;
+// ============================================================
 
 playBtn.addEventListener("click", function () {
     if (nameInput.value == "") {
@@ -36,8 +45,8 @@ playerPlayBtn.addEventListener("click", function () {
     if (playerOneInput.value == "") {
         playerOneError.style.display = "block";
         playerOneError.innerHTML = "Number is Missing";
-    } 
-    else if(playerOneInput.value <1 || playerOneInput.value > 10)   {
+    }
+    else if (playerOneInput.value < 1 || playerOneInput.value > 10) {
         playerOneError.style.display = "block";
         playerOneError.innerHTML = "Out of Range!";
     }
@@ -45,13 +54,13 @@ playerPlayBtn.addEventListener("click", function () {
         console.log("It's a number");
         playerOnegameArea.style.display = "none";
         playerTwoGameArea.style.display = "block";
-        
+
     }
-    else{
+    else {
         playerOneError.style.display = "block";
         playerOneError.innerHTML = "Invalid input";
     }
-    
+
 });
 // ============================================================
 // ======================= Player Two =========================
@@ -61,9 +70,43 @@ playerTwoPlayBtn.addEventListener("click", function () {
     }
     else {
         playerTwoGameArea.style.display = "none";
-        // playerOnegameArea.style.display = "block";
-        // let h = playerOneHeading.innerHTML + ' ( ' + nameInput.value + ' ) ';
-        // playerOneHeading.innerHTML = h;
+        playerTwoChanceOne.style.display = "block";
+        let playerTwoName = playerTwoHeading.innerHTML + ' ( ' + playerTwoNameInput.value + ' ) ';
+        playerTwoHeading.innerHTML = playerTwoName;
+    }
+});
+// ============================================================
+// ======================= Player Two Chance =========================
+playerTwoChanceOnePlayBtn.addEventListener("click", function () {
+    let playerOneNumber = playerOneInput.value;
+    let playerTwoNumber = playerTwoChanceOneInput.value;
+    
+    
+    if (playerTwoChanceOneInput.value == "") {
+        playerTwoChanceOneError.style.display = "block";
+        playerTwoChanceOneError.innerHTML = "Number is Missing";
+    }
+    else if (!(playerTwoChanceOneInput.value - 15)) {
+        console.log("Not a number");
+        playerTwoChanceOneError.style.display = "block";
+        playerTwoChanceOneError.innerHTML = "Invalid input";
+    }
+    else {       
+        if (playerOneNumber === playerTwoNumber && counter <= 3) {
+            console.log("Matched");
+            playerTwoChanceOne.style.display = "none";
+        }
+        else if (playerOneNumber != playerTwoNumber && counter <= 3) {
+            console.log(counter);
+            counter++;   
+            if(counter <= 3){
+                playerTwoChanceNum.innerHTML = "Chance - " + counter;
+            }         
+        }
+        if(counter > 3){
+            console.log("Game Over");
+            playerTwoChanceOne.style.display = "none";
+        }        
     }
 });
 // ============================================================
